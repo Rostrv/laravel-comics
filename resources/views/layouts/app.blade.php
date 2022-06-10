92449ecc22a75881ce7396521d72cfe5eab6f0a7
Rostrv <trovatorosario91@gmail.com>
2022-06-10 15:51:13 +0200

    ended layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,7 +58,86 @@
         @yield('content')
     </main>
 
-    <footer></footer>
+    <footer>
+        <section class="site_footer">
+            <div class="container down">
+                <div class="row">
+                    <div class="col-6 p-4">
+                        <div class="row">
+                            <div class="col-3">
+                                <h3>dc comics</h3>
+                                <ul>
+                                    <li><a href="">Characters</a></li>
+                                    <li><a href="">Comics</a></li>
+                                    <li><a href="">Movies</a></li>
+                                    <li><a href="">Tv</a></li>
+                                    <li><a href="">Videos</a></li>
+                                    <li><a href="">Games</a></li>
+                                    <li><a href="">News</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-3">
+                                <h3>dc</h3>
+                                <ul>
+                                    <li><a href="">Terms of use</a></li>
+                                    <li><a href="">Privacy policy</a></li>
+                                    <li><a href="">Add choices</a></li>
+                                    <li><a href="">Advertising</a></li>
+                                    <li><a href="">Jobs</a></li>
+                                    <li><a href="">Subscriptions</a></li>
+                                    <li><a href="">Talent workshop</a></li>
+                                    <li><a href="">Cpsc certificate</a></li>
+                                    <li><a href="">Ratings</a></li>
+                                    <li><a href="">Shop help</a></li>
+                                    <li><a href="">Contact us</a></li>
+                                </ul>
+
+                            </div>
+                            <div class="col-3">
+                                <h3>sites</h3>
+                                <ul>
+                                    <li><a href="">DC</a></li>
+                                    <li><a href="">Mad Magazine</a></li>
+                                    <li><a href="">DC Kids</a></li>
+                                    <li><a href="">DC Univers</a></li>
+                                    <li><a href="">DC Power Visa</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-3">
+                                <h3>shop</h3>
+                                <ul>
+                                    <li><a href="">Shop DC</a></li>
+                                    <li><a href="">Shop DC Collectibles</a></li>
+                                </ul>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-6 logo">
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <div class="container bottom">
+                    <button>
+                        SIGN UP NOW!
+                    </button>
+                    <div class="right d-flex align-items-center">
+                        <h4>follow us</h4>
+                        <ul class=" icons d-flex align-items-center m-0">
+                            <li><a href=""><img src="/img/footer-facebook.png" alt=""></a></li>
+                            <li><a href=""><img src="/img/footer-twitter.png" alt=""></a></li>
+                            <li><a href=""><img src="/img/footer-youtube.png" alt=""></a></li>
+                            <li><a href=""><img src="/img/footer-pinterest.png" alt=""></a></li>
+                            <li><a href=""><img src="/img/footer-periscope.png" alt=""></a></li>
+
+                        </ul>
+                    </div>
+                </div>
+
+
+            </div>
+    </footer>
     <script src="{{asset('js/app.js')}}"></script>
 </body>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,40 @@
 
 @section('content')
 
+<section>
+    <div class="container mb-3 position-relative">
+        <div class="position-absolute up"><button class="btn-load text-uppercase mb-3">current series</button></div>
+
+        <div class="row justify-content-evenly">
+            @foreach ($comics as $index => $comic)
+            <div class="col-2 p-0 text-center">
+                <div class="comic-container" style="width: 200px">
+                    <img class="cover" src="{{ $comic['thumb'] }}">
+                    <div class="p-1">
+                        <h5> {{ $comic['series'] }} </h5>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+
+    <div class="d-flex justify-content-center">
+        <button class="btn-load text-uppercase mb-3">load more</button>
+    </div>
+    <div class="middle">
+        <div class="container blue">
+            <ul class="p-0 m-0">
+                <li> <a href=""> <img src="/img/buy-comics-digital-comics.png" alt=""> digital comics</a></li>
+                <li> <a href=""> <img src="/img/buy-comics-merchandise.png" alt="">dc merchandise</a> </li>
+                <li> <a href=""> <img src="/img/buy-comics-shop-locator.png" alt="">subscription</a> </li>
+                <li> <a href=""> <img src="/img/buy-comics-subscriptions.png" alt="">comic shop locator</a> </li>
+                <li> <a href=""> <img src="/img/buy-dc-power-visa.svg" alt="">dc power visa</a> </li>
+            </ul>
+        </div>
+    </div>
+
+</section>
 
 
 @endsection
